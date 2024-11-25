1d56de293c8f768c0d7b755def11edb8226c218d
Krystian Krupiński <krupinskikrycha@gmail.com>
2024-11-25 19:19:13 +0100

    added theme, GlobalStyles and themeProvider

---
+++ src/GlobalStyles.js
@@ -0,0 +1,18 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+html {
+  box-sizing: border-box;
+}
+* {
+  *, *::before, *::after {
+  box-sizing: inherit;
+}
+}
+
+body {
+  font-family: 'Montserrat', sans-serif;
+  background-color: ${({theme}) => theme.colors.mercury};
+  word-break: break-word;
+}
+`;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { theme } from './theme';
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyle } from './GlobalStyles';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+   <ThemeProvider theme={theme}>
+  <GlobalStyle/>
+   <App />
+   </ThemeProvider>
   </React.StrictMode>
 );
 
