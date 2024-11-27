2eb36ebe99133d997a897cf46d785cdd43a0df99
Krystian Krupiński <krupinskikrycha@gmail.com>
2024-11-27 20:34:30 +0100

    moved theme from core for better accessibilty

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { theme } from './core/App/theme';
+import { theme } from './theme';
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyle } from './core/App/GlobalStyles';
 import reportWebVitals from './reportWebVitals';
