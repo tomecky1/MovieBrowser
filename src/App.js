11da4681af35858efdc0fb0f6d9a454b2c2dd92d
Krystian Krupiński <krupinskikrycha@gmail.com>
2024-11-27 21:12:57 +0100

    added new index.js to Header component and styled.js for simplicity in App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import { Container } from "./common/Container";
-import { AdditionalInfo, Header, HeaderImage } from "./common/Header";
-import poster3 from "./image/poster3.jpg"
+import { Image } from "./common/Header/styled";
+import Header from "./common/Header";
+import poster3 from "./image/poster3.jpg";
+import Error from "./Error";
 
 function App() {
   return (
     <Container>
-      <Header>
-        <HeaderImage src={poster3} alt=""/>
-        <AdditionalInfo>
-            Nazwa filmu <br/>
-            Ocena <br/>
-            Wyświetlenia
-          </AdditionalInfo>
+      <Header 
+      title = {"Nazwa filmu"}
+      rating = {"Ocena"}
+      votes = {"Głosy"}
+      >
+        <Image src={poster3} alt="" />
       </Header>
+      <Error></Error>
     </Container>
   );
 }
