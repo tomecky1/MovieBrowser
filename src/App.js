593a4bd6af41914a3ae7155c80259ac7e51ec272
Krystian Krupiński <krupinskikrycha@gmail.com>
2024-11-27 18:53:19 +0100

    added Header, Container, styles to it and put that in App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+import { Container } from "./common/Container";
+import { AdditionalInfo, Header, HeaderImage } from "./common/Header";
+import poster3 from "./image/poster3.jpg"
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Container>
+      <Header>
+        <HeaderImage src={poster3} alt=""/>
+        <AdditionalInfo>
+            Nazwa filmu <br/>
+            Ocena <br/>
+            Wyświetlenia
+          </AdditionalInfo>
+      </Header>
+    </Container>
   );
 }
 
---
+++ src/common/Container/index.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const Container = styled.main`
+  max-width: 1920px;
+  margin: auto;
+  background-color: ${({ theme }) => theme.color.white};
+`;
