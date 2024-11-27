373730dd400bfafcbf88b60eeafbf637ed650de6
Krystian Krupiński <krupinskikrycha@gmail.com>
2024-11-27 21:18:51 +0100

    added Error page Component

---
+++ src/Error/index.js
@@ -0,0 +1,17 @@
+import { Wrapper, Button, Header2 } from "./styled";
+import danger from "../image/danger.png";
+
+const Error = () => {
+  return (
+    <>
+      <Wrapper>
+        <img src={danger} alt="" />
+        <Header2>Ooops! Something went wrong...</Header2>
+        <p>Please check your network connection and try again</p>
+        <Button>Back to homepage</Button>
+      </Wrapper>
+    </>
+  );
+};
+
+export default Error;
---
+++ src/Error/styled.js
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  
+@media (max-width: ${({theme}) => theme.breakpoint.mobileMax}px) {
+  text-align: center;
+}
+`;
+
+export const Button = styled.button`
+  padding: 16px 24px;
+  color: ${({ theme }) => theme.color.white};
+  background-color: ${({ theme }) => theme.color.scienceBlue};
+  font-size: 14px;
+  line-height: 19px;
+`;
+
+export const Header2 = styled.h2`
+margin: 0;
+`
