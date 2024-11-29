ff2ac86e76500bd9e6280fc25f5c7c9d75cca935
Albert Ciesla <albert383@gmail.com>
2024-11-29 20:08:18 +0100

    Add MovieDetails component to App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {Navigation} from "./common/Navigation"
 import {Image } from "./common/Header/styled.js";
 import Header from "./common/Header/index.js";
 import poster3 from "./image/poster3.jpg"
+import { MovieDetails } from "./features/MovieDetails/index.js";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
       >
         <Image src={poster3} alt="" />
       </Header>
+      <MovieDetails />
     </Container>
   );
 }
