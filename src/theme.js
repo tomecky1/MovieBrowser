1e31b579faab514b78208f39ac096402cd29bb6e
Albert Ciesla <albert383@gmail.com>
2024-11-29 20:08:33 +0100

    Update theme

--- src/theme.js
+++ src/theme.js
@@ -2,10 +2,14 @@ export const theme = {
   color: {
     white: "#ffffff",
     whisper: "#f5f5fa",
-    scienceBlue: "#0044cc"
+    scienceBlue: "#0044cc",
+    mystic: "#E4E6F0", // movie kind containter
+    candleLight: "#FCD420", //star rate SVG
+    stormGray: "#74788B", // text "Production", "Release Date",
+    woodSmoke: "#18181B", // movie kind "text- Action, Drama etc"
   },
 
-  breakpoint: {
-    mobileMax: 440,
+  breakpoints: {
+    mobileMax: "440px",
   }
 };
