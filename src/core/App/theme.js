b5bd634e4fb85ac8defb7cd2ad8687e09f5852ea
Albert Ciesla <albert383@gmail.com>
2024-11-29 17:20:13 +0100

    Theme updated

--- src/core/App/theme.js
+++ src/core/App/theme.js
@@ -1,5 +1,17 @@
 export const theme = {
     color: {
-        white: "#FFFFFF"
+        white: "#FFFFFF",
+        black: "#000000",
+        mystic: "#E4E6F0", // movie kind containter
+        candleLight: "#FCD420", //star rate SVG
+        stormGray: "#74788B", // text "Production", "Release Date",
+        woodSmoke: "#18181B", // movie kind "text- Action, Drama etc"
+    }
+};
+export const devices = {
+    breakpoints: {
+        mobile: "320px",
+        tabletVertical: "991px",
+        tabletHorizontal: "1199px"
     }
 }
