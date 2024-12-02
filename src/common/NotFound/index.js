79a86428638bb3c7df848180267526bc068b0fbc
Krystian Krupiński <krupinskikrycha@gmail.com>
2024-12-02 18:56:31 +0100

    moved to common

---
+++ src/common/NotFound/index.js
@@ -0,0 +1,17 @@
+import { Wrapper, Text, ImageWrapper } from "./styled";
+import Picture from "../../image/Picture.png";
+
+const NotFound = () => {
+  return (
+    <>
+      <Wrapper>
+        <Text>Sorry, there are no results for "Muan"</Text>
+        <ImageWrapper>
+          <img src={Picture} alt="" />
+        </ImageWrapper>
+      </Wrapper>
+    </>
+  );
+};
+
+export default NotFound;
