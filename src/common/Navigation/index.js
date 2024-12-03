d2f30e69d4088c7454b38f7a25da80a31bc212a2
Tomasz Piotrowski <tpiotrowski007@gmail.com>
2024-12-03 22:02:14 +0100

    added Routing to navigation

--- src/common/Navigation/index.js
+++ src/common/Navigation/index.js
@@ -1,3 +1,4 @@
+import React from "react";
 import {
   NavigationInput,
   NavigationItem,
@@ -8,22 +9,31 @@ import {
 } from "./styled";
 import Video from "../../image/Video.png";
 import {ReactComponent as SearchIcon} from "../../icons/Search.svg";
+import {BrowserRouter, NavLink, Route, Routes} from "react-router-dom";
+import {MovieDetails} from "../../features/MovieDetails";
+import {Person} from "../../features/Person";
 
 export function Navigation() {
   return (
     <>
-      <NavigationWrapper>
-        <NavigationList>
-          <img src={Video} alt="Logo"/>
-          <NavigationTitle>Movies Browser</NavigationTitle>
-          <NavigationItem>MOVIES</NavigationItem>
-          <NavigationItem>PEOPLE</NavigationItem>
-          <SearchIconWrapper>
-            <SearchIcon/>
-            <NavigationInput type="text" placeholder={"Search for movies..."}></NavigationInput>
-          </SearchIconWrapper>
-        </NavigationList>
-      </NavigationWrapper>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <NavigationWrapper>
+          <NavigationList>
+            <img src={Video} alt="Logo"/>
+            <NavigationTitle>Movies Browser</NavigationTitle>
+            <NavigationItem><NavLink to={"/movie"} activeClassName={"active"}>MOVIES</NavLink></NavigationItem>
+            <NavigationItem><NavLink to={"/person"} activeClassName={"active"}>PEOPLE</NavLink></NavigationItem>
+            <SearchIconWrapper>
+              <SearchIcon/>
+              <NavigationInput type="text" placeholder={"Search for movies..."}></NavigationInput>
+            </SearchIconWrapper>
+          </NavigationList>
+          <Routes>
+            <Route exact path="/movie" element={<MovieDetails/>}/>
+            <Route path={"/person"} element={<Person/>}/>
+          </Routes>
+        </NavigationWrapper>
+      </BrowserRouter>
     </>
   );
 }
