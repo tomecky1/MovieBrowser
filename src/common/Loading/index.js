63715978c52615403b38e47ddad7e86157db1ba6
Krystian Krupiński <krupinskikrycha@gmail.com>
2024-12-03 19:16:56 +0100

    code formatted

--- src/common/Loading/index.js
+++ src/common/Loading/index.js
@@ -1,12 +1,12 @@
 import { Wrapper, Text, Loader } from "./styled";
 
 const Loading = () => {
-return (
-         <Wrapper>
-        <Text>Search results for "Mulan"</Text>
-        <Loader/>
-      </Wrapper>
-)
+  return (
+    <Wrapper>
+      <Text>Search results for "Mulan"</Text>
+      <Loader />
+    </Wrapper>
+  );
 };
 
 export default Loading;
