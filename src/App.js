a1161b71a4950d7406478b145380462da9d49c91
Krystian Krupiński <krupinskikrycha@gmail.com>
2024-12-05 16:32:26 +0100

    moved passed values to index.js

--- src/App.js
+++ src/App.js
@@ -2,15 +2,12 @@ import { Container } from "./common/Container";
 import Header from "./common/Header/index.js";
 import { MovieDetails } from "./features/MovieDetails/index.js";
 import { Navigation } from "./common/Navigation";
-import { Image } from "./common/Header/styled.js";
-import poster3 from "./image/poster3.jpg";
 
 function App() {
   return (
     <Container>
       <Navigation />
-      <Header title={"Nazwa filmu"} rating={"Ocena"} votes={"Wyświetlenia"}>
-        <Image src={poster3} alt="" />
+      <Header>
       </Header>
       <MovieDetails />
     </Container>
