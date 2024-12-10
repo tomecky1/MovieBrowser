147b8954c2e48bc6c33679ae8a0b538dfc20bc5c
Krystian Krupiński <krupinskikrycha@gmail.com>
2024-12-10 21:12:58 +0100

    added CastTile component to App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Container } from "./common/Container";
+import CastTile from "./features/CastTile";
 import Header from "./common/Header/index.js";
 import { MovieDetails } from "./features/MovieDetails/index.js";
 import { Navigation } from "./common/Navigation";
@@ -7,9 +8,9 @@ function App() {
   return (
     <Container>
       <Navigation />
-      <Header>
-      </Header>
+      <Header></Header>
       <MovieDetails />
+      <CastTile />
     </Container>
   );
 }
