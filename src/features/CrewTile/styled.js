08e687f86cfe4db7d3d03529cfe95a585ce48012
Krystian Krupiński <krupinskikrystian01@gmail.com>
2024-12-12 18:04:50 +0100

    Update styled.js

--- src/features/CrewTile/styled.js
+++ src/features/CrewTile/styled.js
@@ -91,7 +91,7 @@ export const WrapperRole = styled.p`
   line-height: 27px;
   text-align: center;
   margin: 0;
-  color: ${({ theme }) => theme.color.darkerGrey};
+  color: ${({ theme }) => theme.color.waterloo};
   padding-bottom: 16px;
   @media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
     font-size: 13px;
