e6aa14fa232c32bbea15d8c80e555c6538b29503
Tomasz Piotrowski <tpiotrowski007@gmail.com>
2024-12-15 13:32:45 +0100

    Fetch and display movie credits in CastAndCrew component; handle errors and update API options for credits retrieval.

--- src/features/movieDetailsAPI.js
+++ src/features/movieDetailsAPI.js
@@ -1,22 +1,25 @@
-import axios from 'axios';
+import axios from "axios";
 
 const options = {
-  method: 'GET',
-  url: 'https://api.themoviedb.org/3/movie/550',
-  params: {append_to_response: 'images', language: 'en-US'},
+  method: "GET",
+  url: "https://api.themoviedb.org/3/movie/550",
+  params: {
+    append_to_response: "images, configuration, credits",
+    language: "en-US",
+  },
   headers: {
-    accept: 'application/json',
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxNDU0OTgwYWZmZjFjMGJhOWRjZTdlNjIwMmE5ZWNiZiIsIm5iZiI6MTczMzc1OTk5NS4yOTUsInN1YiI6IjY3NTcxM2ZiNDMxMTA4YWEyM2QxMzY0MiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.B2tPfeIoWU9kJVuXZvb-89UGhwjNzgIFISXq4G4zEyc'
-  }
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxNDU0OTgwYWZmZjFjMGJhOWRjZTdlNjIwMmE5ZWNiZiIsIm5iZiI6MTczMzc1OTk5NS4yOTUsInN1YiI6IjY3NTcxM2ZiNDMxMTA4YWEyM2QxMzY0MiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.B2tPfeIoWU9kJVuXZvb-89UGhwjNzgIFISXq4G4zEyc",
+  },
 };
 
 export const getMovieDetails = async () => {
   axios
     .request(options)
-    .then(res => console.log(res.data))
-    .catch(err => console.error(err));
+    .then((res) => console.log(res.data))
+    .catch((err) => console.error(err));
 };
-
 
 export const getMovieOverview = async (movieId) => {
   if (!movieId) {
@@ -35,13 +38,31 @@ export const getMovieOverview = async (movieId) => {
       date: response.data.release_date,
       votes: response.data.vote_count,
       vote_average: response.data.vote_average,
-      poster: response.data.poster_path
+      poster: response.data.poster_path,
     };
   } catch (err) {
     console.error("Błąd podczas pobierania danych filmu:", err);
     return null;
   }
-
 };
 
+export const getMovieCredits = async (movieId) => {
+  if (!movieId) {
+    console.error("movieId nie został dostarczony.");
+    return null;
+  }
 
+  try {
+    const response = await axios.request({
+      ...options,
+      url: `https://api.themoviedb.org/3/movie/${movieId}/credits`,
+    });
+    return response.data.cast.map((actor) => ({
+      name: actor.name,
+      character: actor.character,
+    }));
+  } catch (err) {
+    console.error("Błąd podczas pobierania danych obsady filmu:", err);
+    return null;
+  }
+};
