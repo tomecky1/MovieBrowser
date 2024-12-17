28015fa247f62b1c70f8b416948421b74389599d
Tomasz Piotrowski <tpiotrowski007@gmail.com>
2024-12-17 23:40:19 +0100

    Fix active class name in StyledNavLink and adjust styles for better responsiveness

--- src/common/Navigation/StyledNavLink/styled.js
+++ src/common/Navigation/StyledNavLink/styled.js
@@ -1,26 +1,30 @@
 import styled from "styled-components";
 import { NavLink } from "react-router-dom";
 
-const activeClassName = "active";
+const activeclassname = "active";
 
-export const StyledNavLink = styled(NavLink).attrs(() => ({ activeClassName }))`
+export const StyledNavLink = styled(NavLink).attrs(() => ({ activeclassname }))`
   text-decoration: none;
   color: ${({ theme }) => theme.color.white};
-  margin: 0 16px;
+  /* margin: 0 16px;
   padding: 8px 24px;
   border-radius: 24px;
-  transition: all 0.3s ease;
+  transition: all 0.3s ease; */
 
-  &.active {
+  &.${activeclassname} {
     color: ${({ theme }) => theme.color.white};
     border: 1px solid ${({ theme }) => theme.color.white};
     text-decoration: none;
+    margin: 0 16px;
+    padding: 8px 24px;
     background-color: ${({ theme }) => theme.color.black};
     cursor: pointer;
+    border-radius: 24px;
   }
 
   @media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
-    padding: 8px 12px;
-    border-radius: 29px;
+    padding: 8px 12px !important;
+    border-radius: 29px !important;
+    margin: unset !important;
   }
 `;
