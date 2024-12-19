dbb650132e04de1a644e4c67cc449ae881f58183
Krystian Krupiński <krupinskikrycha@gmail.com>
2024-12-19 19:19:26 +0100

    commented MovieDetails

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <Container>
       <NaviRoutes />
-      <MovieDetails />
+      {/* <MovieDetails /> */}
       {/* <PersonCredits personId={personId} /> */}
       {/* <Pagination /> */}
     </Container>
