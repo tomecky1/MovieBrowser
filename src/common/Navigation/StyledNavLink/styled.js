aca528a50e4bcf0f30fb535ed5e1f2b54201aead
Albert Ciesla <albert383@gmail.com>
2024-12-19 17:44:55 +0100

    Add new styles to StyledNavLink

--- src/common/Navigation/StyledNavLink/styled.js
+++ src/common/Navigation/StyledNavLink/styled.js
@@ -9,7 +9,16 @@ export const StyledNavLink = styled(NavLink).attrs(() => ({
   text-decoration: none;
   color: ${({ theme }) => theme.color.white};
   border-radius: 24px;
-  border: 1px transparent;
+  border: 1px black;
+  font-weight: 600;
+  font-size: 14px;
+  padding: 8px 25px;
+  transition: transform ease 500ms, border 0.5s ease, margin 0.5s ease; 
+
+  &:hover {
+        border: 1px solid ${({ theme }) => theme.color.white};
+        border-radius: 24px;
+    }
 
   &.${activeclassname} {
     color: ${({ theme }) => theme.color.white};
@@ -18,6 +27,7 @@ export const StyledNavLink = styled(NavLink).attrs(() => ({
     padding: 8px 24px;
     background-color: ${({ theme }) => theme.color.black};
     cursor: pointer;
+    border-radius: 24px;
   }
 
   @media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
