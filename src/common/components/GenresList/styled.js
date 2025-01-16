be24b33aea8cf40f986cfc7f2fcd4f0ce675e7a2
Krystian Krupiński <krupinskikrycha@gmail.com>
2025-01-16 17:13:36 +0100

    Genres got font size changed to 14px

--- src/common/components/GenresList/styled.js
+++ src/common/components/GenresList/styled.js
@@ -5,13 +5,12 @@ export const TagsList = styled.ul`
   flex-wrap: wrap;
   list-style: none;
   padding-left: 0;
-  margin: -8px;
+  margin: 0;
+  gap: 8px;
 
   @media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
     font-size: 10px;
     line-height: 11px;
-    margin: 0;
-    gap: 8px;
   }
 `;
 
@@ -19,11 +18,9 @@ export const Genre = styled.li`
   color: ${({ theme }) => theme.color.black};
   background-color: ${({ theme }) => theme.color.mystic};
   font-weight: 400;
-  font-size: 18px;
+  font-size: 14px;
   border-radius: 5px;
   padding: 8px 16px;
-  font-size: 10px;
-  margin: 8px;
 
   @media (max-width: ${({ theme }) => theme.breakpoints.tabletVertical}) {
     font-size: 16px;
