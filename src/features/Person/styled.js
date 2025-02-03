cd8a65b8b9660e1a0a88e9ca9f636d6c5612e88e
Tomasz Piotrowski <tpiotrowski007@gmail.com>
2025-02-03 21:18:19 +0100

    Refactor SearchResults component: update movie state structure, enhance result display, and improve responsive design for person search results.

--- src/features/Person/styled.js
+++ src/features/Person/styled.js
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
 
 export const StyledPersonLink = styled(Link)`
   text-decoration: none;
@@ -35,21 +35,38 @@ export const Text = styled.header`
   @media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
     font-size: 20px;
     line-height: 24px;
-    margin: 24px 0 16px 0px;
+    margin: 24px 0 16px 0;
   }
 `;
 
 export const StyledPersonWrapper = styled.div`
   background-color: ${({ theme }) => theme.color.whisper};
   display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(208px, 1fr));
+  grid-template-columns: repeat(6, minmax(208px, 1fr));
   grid-template-rows: repeat(4, minmax(339px, 1fr));
   gap: 24px;
 
-  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
-    grid-template-columns: repeat(auto-fit, minmax(136px, 1fr));
-    grid-template-rows: repeat(2, minmax(245px, 1fr));
+  @media (max-width: ${({ theme }) => theme.breakpoints.nextHubMax}) {
+    grid-template-columns: repeat(5, minmax(208px, 1fr));
     gap: 16px;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.tabletHorizontal}) {
+    grid-template-columns: repeat(4, minmax(208px, 1fr));
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.tabletVerticalS}) {
+    grid-template-columns: repeat(3, minmax(208px, 1fr));
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.biggerMobile}) {
+    grid-template-columns: repeat(3, minmax(136px, 1fr));
+    grid-template-rows: repeat(2, minmax(245px, 1fr));
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.smallMobile}) {
+    grid-template-columns: repeat(2, minmax(136px, 1fr));
+    grid-template-rows: repeat(2, minmax(245px, 0fr));
   }
 `;
 
@@ -64,9 +81,9 @@ export const WrapperItem = styled.div`
   @media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
     padding: 8px;
     border-radius: 5px;
-    box-shadow: 0px 4px 12px 0px ${({ theme }) => theme.color.heather};
+    box-shadow: 0 4px 12px 0 ${({ theme }) => theme.color.heather};
   }
-    
+
   &:hover {
     transform: scale(1.03);
   }
